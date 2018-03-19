61af670cca921c4cdd3aad85b76451025cb707f9
Maverick Tesini <mavericktesini@gmail.com>
2018-03-19 15:08:01 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,6 +15,11 @@ class String
   end
 
   def count_sentences
-
+    #split string into array elements based on whitespaces
+    #check each element to see if it ends with "." "?" "!"
+    #count those elements....
+    #could shovel them into new array and use array length
+    #or could use a counter.
+    # OR could use array.count {|element| booleanequation} => # of true
   end
 end
